de18b9bd863aa4d3c9780e9f2dfda712221378ec
23Bas <agung.ngurah145@gmail.com>
2023-05-30 16:11:09 +0700

    Kelayakan

--- jalan_2021/urls.py
+++ jalan_2021/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . views.jenis import index as jenis_index
+from . views.kelayakan import index as kelayakan_index
 
 urlpatterns = [
-    path('jenis', jenis_index, name='jenis_index')
+    path('jenis', jenis_index, name='jenis_index'),
+    path('kelayakan', kelayakan_index, name='kelayakan_index')
 ]
